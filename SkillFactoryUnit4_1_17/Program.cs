41fe3378fa70b42a4cd31cfbada845b79f434a79
Jaroslaw Zembitski <yaroslavfox@outlook.com>
2023-07-20 11:23:18 +0500

    Добавьте файлы проекта.

---
+++ SkillFactoryUnit4_1_17/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Your favorite color?");
+
+        var color = Console.ReadLine();
+
+        if (color == "yellow")
+        {
+            Console.BackgroundColor = ConsoleColor.DarkYellow;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Your color is yellow!");
+        }
+
+        else if (color == "green")
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Your color is green!");
+        }
+        else
+        {
+            Console.BackgroundColor = ConsoleColor.DarkYellow;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("No no no. I'm sure your favorite color is yellow!");
+        }
+    }
+}
